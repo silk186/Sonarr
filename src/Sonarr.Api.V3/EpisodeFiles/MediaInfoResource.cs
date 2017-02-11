edb9f99611278a0e46b453979d2ca88f7b7f85d6
Mark McDowall <markus.mcd5@gmail.com>
2017-02-10 22:46:39 -0800

    v3 API

---
+++ src/Sonarr.Api.V3/EpisodeFiles/MediaInfoResource.cs
@@ -0,0 +1,25 @@
+﻿using NzbDrone.Core.MediaFiles.MediaInfo;
+using Sonarr.Http.REST;
+
+namespace Sonarr.Api.V3.EpisodeFiles
+{
+    public class MediaInfoResource : RestResource
+    {
+        public decimal AudioChannels { get; set; }
+        public string AudioCodec { get; set; }
+        public string VideoCodec { get; set; }
+    }
+
+    public static class MediaInfoResourceMapper
+    {
+        public static MediaInfoResource ToResource(this MediaInfoModel model, string sceneName)
+        {
+            return new MediaInfoResource
+                   {
+                       AudioChannels = MediaInfoFormatter.FormatAudioChannels(model),
+                       AudioCodec = MediaInfoFormatter.FormatAudioCodec(model),
+                       VideoCodec = MediaInfoFormatter.FormatVideoCodec(model, sceneName)
+                   };
+        }
+    }
+}
